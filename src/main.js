f3bdc9c00f9ce6597c8f4fb42145d84c35fccd98
deshuge7@gmail.com <Des465688>
2021-08-02 22:31:15 +0300

    Добавил дефолтное значение для рендера

--- src/main.js
+++ src/main.js
@@ -7,36 +7,36 @@ import {createShowMoreButton} from './views/show-more.js';
 import {createFilmDetailsPopup} from './views/film-details-popup.js';
 import {createFilmsStatisticTemplate} from './views/films-statistic.js';
 
-const render = (container, template, place) => {
+const render = (container, template, place = 'beforeend') => {
   container.insertAdjacentHTML(place, template);
 };
 
 const header = document.querySelector('.header');
-render(header, createProfileRatingTemplate(), 'beforeend');
+render(header, createProfileRatingTemplate());
 
 const main = document.querySelector('.main');
 render(main, createMenuTemplate(), 'afterbegin');
-render(main, createSortTemplate(), 'beforeend');
-render(main, createFilmsTemplate(), 'beforeend');
+render(main, createSortTemplate());
+render(main, createFilmsTemplate());
 
 const filmsList = document.querySelector('.films .films-list__container');
-render(filmsList, createFilmCardTemplate(), 'beforeend');
-render(filmsList, createFilmCardTemplate(), 'beforeend');
-render(filmsList, createFilmCardTemplate(), 'beforeend');
-render(filmsList, createFilmCardTemplate(), 'beforeend');
-render(filmsList, createFilmCardTemplate(), 'beforeend');
+render(filmsList, createFilmCardTemplate());
+render(filmsList, createFilmCardTemplate());
+render(filmsList, createFilmCardTemplate());
+render(filmsList, createFilmCardTemplate());
+render(filmsList, createFilmCardTemplate());
 render(filmsList, createShowMoreButton(), 'afterend');
 
 const filmsTopRated = document.querySelector('.top-rated');
-render(filmsTopRated, createFilmCardTemplate(), 'beforeend');
-render(filmsTopRated, createFilmCardTemplate(), 'beforeend');
+render(filmsTopRated, createFilmCardTemplate());
+render(filmsTopRated, createFilmCardTemplate());
 
 const filmsMostCommented = document.querySelector('.most-commented');
-render(filmsMostCommented, createFilmCardTemplate(), 'beforeend');
-render(filmsMostCommented, createFilmCardTemplate(), 'beforeend');
+render(filmsMostCommented, createFilmCardTemplate());
+render(filmsMostCommented, createFilmCardTemplate());
 
 const filmStatistic = document.querySelector('.footer__statistics');
-render(filmStatistic, createFilmsStatisticTemplate(), 'beforeend');
+render(filmStatistic, createFilmsStatisticTemplate());
 
 const footer = document.querySelector('.footer');
 render(footer, createFilmDetailsPopup(), 'afterend');
